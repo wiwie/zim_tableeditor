f3ee1d326297d044466e02ef980bd10d8e31ddd7
wiwiec <derwiwie@googlemail.com>
2015-04-18 11:18:55 +0200

    * bugfix: after last changes, simple insert of a new table did not work anymore, only edit ;-) * replaced buttons / comboboxes to delete rows and columns by right click context menus

--- tableeditor.py
+++ tableeditor.py
@@ -80,11 +80,12 @@ class MainWindowExtension(WindowExtension):
 	@action(_('Ta_ble...')) # T: menu item
 	def insert_table(self):
 		'''Run the InsertTableDialog'''
-		lang = InsertTableDialog(self.window, self.window.pageview).run()
+		obj = TableObject({'type': OBJECT_TYPE}, '|Column 1|Column 2|\nRow 1|||\nRow 2|||\n', self.window.ui) # XXX
+		dialog = InsertTableDialog(self.window, self.window.pageview, table=obj, edit=False)
+		lang = dialog.run()
 		if not lang:
 			return # dialog cancelled
 		else:
-			obj = TableObject({'type': OBJECT_TYPE}, '', self.window.ui) # XXX
 			pageview = self.window.pageview
 			pageview.insert_object(pageview.view.get_buffer(), obj)
 
@@ -94,15 +95,16 @@ class InsertTableDialog(Dialog):
 	scriptname = 'table.tex'
 	imagename = 'table.png'
 
-	def __init__(self, ui, pageview, table=None):
-		if table is None:
+	def __init__(self, ui, pageview, table=None, edit=False):
+		if not edit:
 			Dialog.__init__(self, ui, _('Insert Table'), # T: Dialog title
 				button=(_('_Insert'), 'gtk-ok'),  # T: Button label
-				defaultwindowsize=(350, 400) )
+				buttons=gtk.BUTTONS_OK_CANCEL)
 		else:
 			Dialog.__init__(self, ui, _('Edit Table'), # T: Dialog title
 				button=(_('_Edit'), 'gtk-ok'),  # T: Button label
-				defaultwindowsize=(350, 400) )
+				buttons=gtk.BUTTONS_OK_CANCEL)
+		self.edit = edit
 		self.table = table
 		self.pageview = pageview
 		
@@ -115,10 +117,8 @@ class InsertTableDialog(Dialog):
 		self.rowIds = [1,2]
 		self.colIds = [1,2]
 		if table is not None:
-			self.edit = True
 			self.init_table(table=self.table)
 		else:
-			self.edit = False
 			self.init_table()
 			
 	# if no new columns are specified, we just return a copy of the old store
@@ -130,7 +130,10 @@ class InsertTableDialog(Dialog):
 		for c in range(0,storeOld.get_n_columns()):
 			columnTypes.append(storeOld.get_column_type(c))
 			
-		columnTypes.extend(newColumnTypes)
+		#columnTypes.extend(newColumnTypes)
+		for newColumnType in newColumnTypes:
+			columnTypes.insert(newColumnType[1], newColumnType[0])
+		
 		store = gtk.ListStore(*columnTypes)
 		# copy rows
 		r = 0
@@ -139,7 +142,7 @@ class InsertTableDialog(Dialog):
 			for value in row:
 				newRow.append(value)
 			for newColumn in newColumnTypes:
-				newRow.append('')
+				newRow.insert(newColumn[1], '')
 			store.append(newRow)
 			if r > 0:
 				rowNames.append([newRow[0]])
@@ -147,7 +150,7 @@ class InsertTableDialog(Dialog):
 				for p in range(1,len(row)):
 					columnNames.append([newRow[p]])
 				for newColumn in newColumnTypes:
-					columnNames.append([''])
+					columnNames.insert(newColumn[1], [''])
 			r = r + 1
 		self.set_store(store)
 		return store, rowNames, columnNames
@@ -162,41 +165,117 @@ class InsertTableDialog(Dialog):
 		self.store.connect('row-changed',self.on_row_changed)
 		self.store.connect('row-deleted',self.on_row_deleted)
 		self.store.connect('row-inserted',self.on_row_inserted)
+		
+	def set_treeview(self, treeview):
+		self.treeview = treeview
+		
+		self.treeview.connect('button-press-event', self.on_button_pressed)
+		
+	def on_button_pressed(self, treeview, event):
+		if event.button == 3:
+			x = int(event.x)
+			y = int(event.y)
+			pthinfo = treeview.get_path_at_pos(x, y)
+			if pthinfo is not None:
+				time = event.time
+				path, col, cellx, celly = pthinfo
+				treeview.grab_focus()
+				treeview.set_cursor( path, col, 0)
+				
+				self.popup = None
+				# show menu entry to delete column
+				# check that we clicked row 0 and that column is not the first (non-editable) column
+				colInd = -1
+				c = 0
+				for column in treeview.get_columns():
+					if column == col:
+						colInd = c
+					c = c + 1
+				if path[0] == 0 and colInd > 0:
+					self.popup = gtk.Menu()
+					win = treeview.get_parent_window()
+							
+					item = gtk.MenuItem(_('Delete Column \'' + self.columnNames[colInd-1][1] + '\''))
+					item.connect_after('activate', 
+						lambda o: self.on_del_col(colInd-1))
+					self.popup.prepend(item)
+					
+					item = gtk.MenuItem(_('Insert Column Left Of \'' + self.columnNames[colInd-1][1] + '\''))
+					item.connect_after('activate', 
+						lambda o: self.on_add_col(colInd))
+					self.popup.prepend(item)
+					
+					item = gtk.MenuItem(_('Insert Column Right Of \'' + self.columnNames[colInd-1][1] + '\''))
+					item.connect_after('activate', 
+						lambda o: self.on_add_col(colInd+1))
+					self.popup.prepend(item)
+
+				if path[0] > 0 and colInd == 0:
+					self.popup = gtk.Menu()
+					win = treeview.get_parent_window()
+							
+					item = gtk.MenuItem(_('Delete Row \'' + self.rownames[path[0]-1][1] + '\''))
+					item.connect_after('activate', 
+						lambda o: self.on_del_row(path[0]-1))
+					self.popup.prepend(item)
+					
+					item = gtk.MenuItem(_('Insert Row Above Of \'' + self.rownames[path[0]-1][1] + '\''))
+					item.connect_after('activate', 
+						lambda o: self.on_add_row(path[0]))
+					self.popup.prepend(item)
+					
+					item = gtk.MenuItem(_('Insert Row Below Of \'' + self.rownames[path[0]-1][1] + '\''))
+					item.connect_after('activate', 
+						lambda o: self.on_add_row(path[0]+1))
+					self.popup.prepend(item)
+					
+				print(self.popup)
+				
+				if self.popup is not None:
+					self.popup.show_all()
+					self.popup.popup( None, None, None, event.button, time)
+			return True
 			
 	def init_table(self, table=None):
 		import gobject
 		
-		treeviewOld = table.treeview
-		storeOld = treeviewOld.get_model()
+		if table is None:
+			storeOld = gtk.ListStore(str, str, str)
+			storeOld.append(['','Column 1','Column 2'])
+			storeOld.append(['Row 1','',''])
+			storeOld.append(['Row 2','',''])
+		else:
+			treeviewOld = table.treeview
+			storeOld = treeviewOld.get_model()
 		# copy model
 		store, rownames, columnames = self.add_columns_to_store(storeOld)
 		
 		self.set_store(store)
 		ncol = store.get_n_columns()
-		self.treeview = gtk.TreeView(store)
-		self.treeview.set_headers_visible(False)
+		treeview = gtk.TreeView(store)
+		treeview.set_headers_visible(False)
 			
-		c = 0
-		for columnOld in treeviewOld.get_columns():
+		for c in range(0,storeOld.get_n_columns()):
 			column = gtk.TreeViewColumn()
 			cellrender = gtk.CellRendererText()
 			cellrender.set_property('editable', True)
 			cellrender.connect('edited', self.edited_cb, (store, c))
 			column.pack_start(cellrender, True)
 			column.add_attribute(cellrender, 'text', c)
-			self.treeview.append_column(column)
-			c = c + 1
+			treeview.append_column(column)
 		
 		table = gtk.Table(rows=2,columns=2,homogeneous=False)
-		table.attach(self.treeview,left_attach=0, right_attach=1, top_attach=0, bottom_attach=1)
+		table.attach(treeview,left_attach=0, right_attach=1, top_attach=0, bottom_attach=1)
 		addRowBtn = gtk.Button("+")
 		addRowBtn.connect('clicked', self.on_add_row)
 		table.attach(addRowBtn,left_attach=0, right_attach=1, top_attach=1, bottom_attach=2)
 		addColBtn = gtk.Button("+")
 		addColBtn.connect('clicked', self.on_add_col)
 		table.attach(addColBtn,left_attach=1, right_attach=2, top_attach=0, bottom_attach=1)
-		self.vbox.add(table)
-		self.treeview.show_all()
+		self.vbox.pack_start(table)
+		treeview.show_all()
+		
+		self.set_treeview(treeview)
 		
 		# buttons / comboboxes for removal of rows/columns
 		table = gtk.Table(rows=2,columns=3,homogeneous=False)
@@ -223,7 +302,9 @@ class InsertTableDialog(Dialog):
 		table.attach(combo,left_attach=1, right_attach=2, top_attach=3, bottom_attach=4)
 		self.tableView = table
 		
-		self.vbox.add(table)
+		# do not add those components - have been replaced by right click menus on columns / rows
+		# TODO: remove those components
+		#self.vbox.add(table)
 		
 	def on_row_changed(self, treemodel, path, iter):
 		self.update_row_names()
@@ -268,14 +349,20 @@ class InsertTableDialog(Dialog):
 		if text.keyval == gtk.keysyms.Tab or text.keyval == gtk.keysyms.ISO_Left_Tab:
 			textview.emit_stop_by_name('key-press-event')
 		
-	def on_add_row(self, button):
+	def on_add_row(self, rowInd):
 		newRow = []
+		r = len(self.treeview.get_model())
+		if not isinstance(rowInd, ( int, long ) ):
+			rowInd = r
+			
 		for c in range(0, self.treeview.get_model().get_n_columns()):
 			newRow.append('')
-		self.treeview.get_model().append(newRow)
+		self.treeview.get_model().insert(rowInd, newRow)
 		self.update_row_names()
 		
-	def on_del_row(self, button):
+	def on_del_row(self, rowInd):
+		if rowInd is not None:
+			self.remove_row_combo.set_active(rowInd)
 		active_row = self.remove_row_combo.get_active_iter()
 		if active_row is None:
 			return
@@ -287,9 +374,12 @@ class InsertTableDialog(Dialog):
 		target_iter = self.treeview.get_model().iter_next(target_iter)
 		self.treeview.get_model().remove(target_iter)
 		
-	def on_add_col(self, button):
+	def on_add_col(self, colInd):
 		c = self.treeview.get_model().get_n_columns()
-		store, rownames, columnames = self.add_columns_to_store(self.treeview.get_model(), newColumnTypes=[gobject.TYPE_STRING])
+		if not isinstance(colInd, ( int, long ) ):
+			colInd = c
+		
+		store, rownames, columnames = self.add_columns_to_store(self.treeview.get_model(), newColumnTypes=[(gobject.TYPE_STRING, colInd)])
 		self.treeview.set_model(store)
 		self.set_store(store)
 		
@@ -303,7 +393,9 @@ class InsertTableDialog(Dialog):
 		self.treeview.append_column(column)
 		
 		
-	def on_del_col(self, button):
+	def on_del_col(self, colInd):
+		if colInd is not None:
+			self.remove_col_combo.set_active(colInd)
 		active_col = self.remove_col_combo.get_active_iter()
 		if active_col is None:
 			return
@@ -324,14 +416,15 @@ class InsertTableDialog(Dialog):
 				if c <> target_col:
 					newRow.append(row[c])
 			newStore.append(newRow)
+		
 		self.treeview.set_model(newStore)
 		self.set_store(newStore)
+		# remove last column
+		self.treeview.remove_column(self.treeview.get_column(self.treeview.get_model().get_n_columns()))
 		
-		self.treeview.remove_column(self.treeview.get_column(target_col))
-
 	def do_response_ok(self):
 		self.table.treeview.set_model(self.treeview.get_model())
-		# TODO: adapt treeview to new columns
+		# adapt treeview to new columns
 		for column in self.table.treeview.get_columns():
 			self.table.treeview.remove_column(column)
 		for c in range(0,self.treeview.get_model().get_n_columns()):
@@ -349,7 +442,7 @@ class InsertTableDialog(Dialog):
 			File(path).remove()
 		
 	def run(self):
-		Dialog.run(self)
+		return Dialog.run(self)
 		
 
 
@@ -365,6 +458,8 @@ class TableObject(CustomObjectClass):
 		self.rowheader = None
 		self.columnheader = None
 		#self.connect('modified-changed', self.dump)
+		
+		self._init_widget()
 
 	def get_widget(self):
 		if not self._widget:
@@ -379,7 +474,6 @@ class TableObject(CustomObjectClass):
 				for value in row:
 					text += value + "|"
 				text += "\n"
-			print(text)
 			return text
 		return self._data
 
@@ -450,7 +544,7 @@ class TableObject(CustomObjectClass):
 			
 	def edit_table(self, pageview, win):
 		'''Run the InsertTableDialog'''
-		lang = InsertTableDialog(win, pageview, table=self).run()
+		lang = InsertTableDialog(win, pageview, table=self, edit=True).run()
 		self.set_modified(True)
 		if not lang:
 			return # dialog cancelled
